dec00fb720d3cf9e434ed8d876430d395f6c5f14
jaumeperello <jaumeperello@gmail.com>
2021-03-30 13:35:16 +0200

    Source Code link updated

--- handlers/start.py
+++ handlers/start.py
@@ -2,52 +2,24 @@ from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from helpers.filters import command
-from helpers.decorators import authorized_users_only
+# from helpers.decorators import authorized_users_only
 
 
 @Client.on_message(command("start"))
-@authorized_users_only
 async def start(_, message: Message):
     await message.reply_text(
         f"""<b>👋🏻 Hi {message.from_user.first_name}!</b>
 
-I am Calls Music, an open-source bot that lets you play music in your groups.
+I am MusiCeros, an open-source bot that lets you play music in your groups.
 
 Use the buttons below to know more about me.""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "⚒ Source code", url="https://github.com/callsmusic/callsmusic"
+                        "⚒ Source code", url="https://github.com/jaumeperello/MusiCeros"
                     )
                 ],
-                [
-                    InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/callsmusicchat"
-                    ),
-                    InlineKeyboardButton(
-                        "Channel 🔈", url="https://t.me/callsmusic"
-                    )
-                ]
             ]
         )
     )
-
-
-# @Client.on_message(command("start") & other_filters)
-# async def start2(_, message: Message):
-#     await message.reply_text(
-#         "💁🏻‍♂️ Do you want to search for a YouTube video?",
-#         reply_markup=InlineKeyboardMarkup(
-#             [
-#                 [
-#                     InlineKeyboardButton(
-#                         "✅ Yes", switch_inline_query_current_chat=""
-#                     ),
-#                     InlineKeyboardButton(
-#                         "No ❌", callback_data="close"
-#                     )
-#                 ]
-#             ]
-#         )
-#     )
