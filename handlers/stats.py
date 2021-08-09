7d38a89fc7eae8efd88b7dea040ad286658cf3cc
sαɴᴊɪᴛ sɪɴʜα <72181891+sanjit-sinha190@users.noreply.github.com>
2021-08-09 12:05:19 +0530

    created a stats command to view bot system details
    /stats command will show ram, CPU and storage usage of the system where it is hosted.

---
+++ handlers/stats.py
@@ -0,0 +1,28 @@
+import sys
+import shutil
+import psutil
+
+from pyrogram import Client
+from helpers.functions import humanbytes
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+@Client.on_message(filters.command(["stats",f"stats@{BOT_USERNAME}"]))
+async def stats(client, message):
+    total, used, free = shutil.disk_usage(".")
+    total = humanbytes(total)
+    used = humanbytes(used)
+    free = humanbytes(free)
+    cpu_usage = psutil.cpu_percent()
+    ram_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    await message.reply_text(
+        text=f"**💫 Bot Stats Of Ani-Music 💫** \n\n**🤖 Bot Version:** `V2.9.1` \n\n**👥 Users:** \n ↳**PM'ed Users:** xyz \n\n**💾 Disk Usage,** \n ↳**Total Disk Space:** `{total}` \n ↳**Used:** `{used}({disk_usage}%)` \n ↳**Free:** `{free}` \n\n**🎛 Hardware Usage,** \n ↳**CPU Usage:** `{cpu_usage}%` \n ↳**RAM Usage:** `{ram_usage}%`",
+        parse_mode="Markdown",
+        quote=True
+    )
+    
+    #----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
